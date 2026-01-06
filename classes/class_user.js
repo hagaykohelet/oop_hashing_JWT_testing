dae009cab64c6fd74a59329c7b17dff007f14bb1
hagay kohelet <hagay2506@gmail.com>
2026-01-06 17:56:39 +0200

    middle of project

---
+++ classes/class_user.js
@@ -0,0 +1,21 @@
+import supabase from "../db/connection_supabase.js"
+
+export default class userdal {
+    static find = async function (username) {
+        const { data, error } = await supabase.from("user").select().eq("username", username)
+        if (error) {
+            return error
+        }
+        if (data.length === 0) {
+            return "not found!"
+        }
+        return data
+    }
+    static add = async function (obj) {
+        const { error } = await supabase.from("user").insert(obj).select()
+        if (error) {
+            return error
+        }
+        return "success"
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,20 @@
 import express from 'express'
 import supabase from '../db/connection_supabase.js'
+import { createToken,verifyToken } from '../utils/token.js'
+import userdal from '../classes/class_user.js'
+
+const authRouter = express()
+
+authRouter.post('/register', async(req, res)=>{
+    try{
+        const newObj = req.body
+    }catch(err){
+        res.status(400).send(String(err))
+    }
+})
 
 
-const authRoter = express()
 
+
+
+export default authRouter
